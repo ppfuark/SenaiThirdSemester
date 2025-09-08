1c6a1ac8c397c32a415d1d88f98dc737ae614a4f
ppfuark <phfuark2007@gmail.com>
2025-09-08 15:45:57 -0300

    SAEB[feat]: Kanban
    Backend api urls

--- SAEB/Kanban/kanban_backend/kanban_backend/settings.py
+++ SAEB/Kanban/kanban_backend/kanban_backend/settings.py
@@ -38,17 +38,23 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'api',
+
+    "corsheaders",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'kanban_backend.urls'
 
